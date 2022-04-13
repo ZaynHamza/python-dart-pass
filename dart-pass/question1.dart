b03eb3b7de6cac82fe0431d1bfb1d23f82b4f6a3
Zayn Hamza <nerovsdante89@gmail.com>
2022-04-13 20:40:26 +0300

    Add files via upload

---
+++ dart-pass/question1.dart
@@ -0,0 +1,17 @@
+
+
+void main() {
+
+  const hour = DateTime.now().hour;  
+  print(hour);
+
+}
+
+//const variables must be created with a constant value and
+//the constructor (DateTime) isn’t a const constructor
+
+
+//void main() {
+
+// int hour = DateTime.now().hour;  
+//  print(hour);}
